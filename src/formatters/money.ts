bcfe481d0210591fc757b424fe74233a11e4742a
Sergei Stralenia <stralenia.s@brainrocket.com>
2024-11-03 13:31:42 +0100

    fix: Money formatting

--- src/formatters/money.ts
+++ src/formatters/money.ts
@@ -1,7 +1,15 @@
 const ONE_MILLION = 1_000_000;
 
 export function formatMoney(value: number, currency: string): string {
+  if (!value) {
+    return 'Цена не указана';
+  }
+
   const valueInMillions = value / ONE_MILLION;
+
+  if (valueInMillions < 0.01) {
+    return 'Цена не указана';
+  }
 
   const formattedNumber = new Intl.NumberFormat('en-EN', {
     maximumFractionDigits: 2,
