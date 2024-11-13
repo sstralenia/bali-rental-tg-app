bf3fad6f3e887dfaa58824bdef86c63cdcd272e1
Sergei Stralenia <stralenia.s@brainrocket.com>
2024-11-13 19:02:22 +0100

    chore: Setup staging env in github pages

--- src/api/apollo-client.ts
+++ src/api/apollo-client.ts
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://bali-rental-bot-eu.hasura.app/v1/graphql',
+    uri: import.meta.env.VITE_GRAPHQL_API_URL,
     headers: {
-      'x-hasura-admin-secret': 'GkyQ61HWG27d4XWmM4SvMLQfbJdGZv7ZqqRi8y58oJMfff26YiRLsWrG0jiN1dZ1',
+      'x-hasura-admin-secret': import.meta.env.VITE_HASURA_ADMIN_SECRET,
     },
   }),
   cache: new InMemoryCache(),
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'public',
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    envDir: 'src/env',
+    base: mode === 'staging' ? '/bali-rental-tg-app/' : '/',
+    plugins: [react()],
+    build: {
+      outDir: 'public',
+    },
+  };
 })
