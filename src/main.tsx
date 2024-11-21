ed9ee6c2770fc531c2f882277d2ee07fe439a9b8
Sergei Stralenia <stralenia.s@brainrocket.com>
2024-11-21 08:24:54 +0100

    chore: Remove eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if (window.visualViewport) {
-  window.visualViewport.addEventListener('resize', () => {
-    // @ts-expect-error Telegram is not a key of window
-    document.body.style.height = window.visualViewport.height + 'px';
-  });
-}
-// This will ensure user never overscroll the page
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) window.scrollTo(0, 0);
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
