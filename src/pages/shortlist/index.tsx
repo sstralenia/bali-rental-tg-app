51ce703999076b4189785aabcc3ffafee50bfe43
Sergei Stralenia <stralenia.s@brainrocket.com>
2024-12-09 12:22:19 +0100

    fix: Bottom insets on devices

--- src/pages/shortlist/index.tsx
+++ src/pages/shortlist/index.tsx
@@ -23,7 +23,7 @@ function ShortlistPage() {
 
   return (
     <Layout>
-      <Container style={{ padding: '20px' }} onScroll={e => console.log(e)}>
+      <Container style={{ padding: '20px', paddingBottom: 'calc(20px + var(--tg-safe-area-inset-bottom))' }}>
         {
           shortlistedProperties.length === 0 && (
             <Center h="calc(100vh - 80px)"><Text>Нет избранных объявлений</Text></Center>
